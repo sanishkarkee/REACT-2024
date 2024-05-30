f9691342352d8a273e6db276523207695fad9012
sanish <s.karki1994@gmail.com>
2024-05-30 17:11:58 +0545

    Video 6: JSX

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,30 @@ const carObj = {
   make: '2008',
 };
 
+let a = 10;
+let msg = '';
+
+if (a > 5) {
+  msg = 'Welcome';
+} else {
+  msg = 'Bye';
+}
+
 function App() {
   return (
     <>
-      <h1>
-        {carObj.name} | {carObj.color} | {carObj.make}
-      </h1>
+      <h1>Ifelse loop is used outside the JSX Or use Ternary Operator</h1>
+      <p>Result of the loop: {msg}</p>
+
+      <h1>Use of Ternary operator inside JSX</h1>
+      <p>{a > 15 ? 'Its valid ' : 'Its not valid'}</p>
+
+      <h1>Object concept.</h1>
+      <p>
+        Result of the Object concept: Car details: {carObj.name} |{' '}
+        {carObj.color} | {carObj.make}
+      </p>
+
       <h1>{name}</h1>
       <h4>{5 + 5}</h4>
       <p>This section is for writing HTML code.</p>
