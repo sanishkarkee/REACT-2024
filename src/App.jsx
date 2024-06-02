bff1b446b04327c887f843ec4f8f453b9db9f1f2
sanish <s.karki1994@gmail.com>
2024-06-02 14:46:38 +0545

    React Props / Props datatype / default props value

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import PropTypes from 'prop-types';
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Sidebar from './Sidebar';
 
-function App() {
+//video 9 - Use of prop-types
+function App(props) {
   return (
     <>
-      <Header />
-      <Sidebar />
-      <Footer />
+      <h1>Hey:{props.name}</h1>
+      <h1>Hey: {props.age}</h1>
+      {/* <h1>Maritial status:{props.isMarried.toString()} </h1>  OR */}
+      <h1>Maritial status:{String(props.isMarried)} </h1>
+      <h1>{props.arr}</h1>
+      <h1>{props.arr[0]}</h1>
     </>
   );
 }
+
+//-----Props datatype  
+App.propTypes = {
+  name: PropTypes.string.isRequired, //props ma name chai aba pass garnai parxa
+  age: PropTypes.number,
+  isMarried: PropTypes.bool,
+  arr: PropTypes.array,
+};
+
+//-----Default props value
+App.defaultProps = {
+  name: 'This is default vale of name',
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Houses from './Houses';
+import App from './App';
 import './index.css';
 
-let a = 'Amir';
+//video 9 -  Use of prop-types
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const friends = ['Ram', 'Shyam', 'Hari', 'Sita'];
+root.render(
   <React.StrictMode>
-    <Houses />
-
-    {/* <HelloMessage name={a} age='29'>
-      <p>children element 1</p>
-    </HelloMessage>
-
-    <HelloMessage name='Puskar' age='30'>
-      <button type='button' value='click'>
-        Click
-      </button>
-    </HelloMessage>
-
-    <HelloMessage name='Pravesh' age='31' /> */}
+    <App name='adil' age={29} isMarried={false} arr={friends} />
   </React.StrictMode>
 );
