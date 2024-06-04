27a04cc4b0c8bc8099efa23c43629ac17ed20609
sanish <s.karki1994@gmail.com>
2024-06-04 14:09:03 +0545

    :sparkles: video 15 - Constructors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-// video 12 & 13 (main.jsx, app.jsx, MyClass.jsx)
-
 class App extends Component {
   render() {
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MyClass } from './MyClass';
+import Student from './Student';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {
-      <MyClass name='Adil' age={23}>
-        <h2>Child props no 1</h2>
-      </MyClass>
-
-      /*<MyClass name='Anit' age={22}>
-      <button type='submit'>Submit</button>
-    </MyClass>
-
-    <MyClass name='Kumar' age={26}>
-      <ol>
-        <li>Ali</li>
-        <li>Zain</li>
-        <li>noman</li>
-      </ol>
-    </MyClass> */
-    }
-
-    {/* <MyClass2 /> */}
+    <Student name='Adil' />
+    {/* Component call garnu bhaneko Object create hunu ho  */}
   </React.StrictMode>
 );
