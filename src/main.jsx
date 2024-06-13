16fcfa809fc3a10f306d1f84b95e287f6d93f0a8
sanish <s.karki1994@gmail.com>
2024-06-13 16:36:29 +0545

    :sparkles: video 18: Event Handlling(Click) with passing props

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Customer from './Customer';
+// import App from './App';
+import ClickFunction from './ClickFunction';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Customer name='Amit' age={23} />
+    <ClickFunction name='Ram' />
   </React.StrictMode>
 );
 /*
