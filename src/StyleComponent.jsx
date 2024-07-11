075bbb69655feb1b0977c6689d1535735eaa8d3d
sanish <notsansh@outlook.com>
2024-07-11 12:57:53 +0545

    Video30 - Inline Styling

--- src/StyleComponent.jsx
+++ src/StyleComponent.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function StyleComponent() {
+  const [state, setState] = useState(false);
+
+  // // Case: 2 & 3;
+  // let obj1 = {
+  //   color: 'blue',
+  //   backgroundColor: 'orange',
+  // };
+
+  // let obj2 = {
+  //   fontSize: '50px',
+  //   fontFamily: 'cooper',
+  // };
+
+  //  Case: 4;
+
   return (
     <>
-      <h1 style={'color:red'}>Learning never ends</h1>
+      {/* Case: 1 - direct inline styling*/}
+      {/* <h1 style={'color:red'}>Learning never ends</h1> */}
+
+      {/* Case: 2 - styling through objects*/}
+      {/* <h1 style={obj1}>Learning never ends.</h1> */}
+
+      {/* Case: 3 - styling through multiple objects*/}
+      {/* <h1 style={{ ...obj1, ...obj2 }}>Learning never ends.</h1> */}
+
+      {/* Case: 4 - styling based on certain conditions using states*/}
+      <button style={obj} onClick={changeState}>
+        Click
+      </button>
     </>
   );
 }
