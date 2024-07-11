3f3913001411daf647f2b4a85f9b68d8befdb1d4
sanish <notsansh@outlook.com>
2024-07-11 12:12:40 +0545

    Video30: Inline Styling

---
+++ src/StyleComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StyleComponent() {
+  return (
+    <>
+      <h1 style={'color:red'}>Learning never ends</h1>
+    </>
+  );
+}
+
+export default StyleComponent;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 import './index.css';
-import Students from './Students';
+import StyleComponent from './StyleComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Students />
+    <StyleComponent />
   </React.StrictMode>
 );
 
@@ -16,7 +16,7 @@ root.render(
 Main point to remember (STATE = object):  ( Class component:State / Functional component: Hooks )
 
 1) Props ko data readonly hunxa, props data ma change garna lai STATE use garinxa
-2) State data are stored as vari ables
+2) State data are stored as variables
 3) State ko data change/modify bhaye paxi component chai re-render or redisplay hunxa
 
 */
